919bd27ef614ba3b9b474eef00ea838a067cff22
NG <guraband@naver.com>
2023-06-30 22:46:12 +0900

    #2.3 Creating Account

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,8 +1,10 @@
+import { authService } from "fbInstance";
 import { React, useState } from "react";
 
 const Auth = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [newAccount, setNewAccount] = useState(true);
     const onChange = (event) => {
         const { target: { name, value } } = event;
 
@@ -12,8 +14,23 @@ const Auth = () => {
             setPassword(value);
         }
     }
-    const onSubmit = (event) => {
+    const onSubmit = async (event) => {
         event.preventDefault();
+        let data;
+        try {
+            if (newAccount) {
+                data = await authService.createUserWithEmailAndPassword(
+                    email, password
+                );
+            } else {
+                data = await authService.signInWithEmailAndPassword(
+                    email, password
+                )
+            }
+            console.log(data);
+        } catch (e) {
+            console.log(e);
+        }
     }
 
     return (
@@ -21,7 +38,7 @@ const Auth = () => {
             <form onSubmit={onSubmit}>
                 <input name="email" type="text" placeholder="Email" required value={email} onChange={onChange} />
                 <input name="password" type="password" placeholder="Password" required value={password} onChange={onChange} />
-                <input type="submit" value="Log In" />
+                <input type="submit" value={newAccount ? "Create Account" : "Log In"} />
             </form>
             <div>
                 <button>Continue with Google</button>

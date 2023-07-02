ff9ddf0f086636abf871aad5c847f0b7a56d3e9a
NG <guraband@naver.com>
2023-07-03 01:18:16 +0900

    기본 form 추가, db 기초 세팅

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,8 +1,20 @@
-import React from "react";
+import { React, useState } from "react";
 
 const Home = () => {
-    return <>
-        <span>Home</span>
+    const [nweet, setNweet] = useState("");
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+    }
+
+    const onChange = () => { }
+
+    return (<>
+        <form onSubmit={onSubmit}>
+            <input type="text" placeholder="What's on your mind?" maxLength={120} />
+            <input type="submit" value="Nweet" />
+        </form>
     </>
+    );
 }
 export default Home;

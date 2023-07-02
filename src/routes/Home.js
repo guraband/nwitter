3cedf4edb4e3522324aeb888f71c3b874e830171
NG <guraband@naver.com>
2023-07-03 01:45:35 +0900

    firestore document add 구현

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,17 +1,28 @@
+import { dbService } from "fbInstance";
 import { React, useState } from "react";
 
 const Home = () => {
     const [nweet, setNweet] = useState("");
 
-    const onSubmit = (event) => {
+    const onSubmit = async (event) => {
         event.preventDefault();
+        await dbService.collection("nweets").add({
+            nweet,
+            createdAt: Date.now()
+        });
+        setNweet("");
     }
 
-    const onChange = () => { }
+    const onChange = (event) => {
+        const {
+            target: { value }
+        } = event;
+        setNweet(value);
+    }
 
     return (<>
         <form onSubmit={onSubmit}>
-            <input type="text" placeholder="What's on your mind?" maxLength={120} />
+            <input type="text" placeholder="What's on your mind?" maxLength={120} onChange={onChange} value={nweet} />
             <input type="submit" value="Nweet" />
         </form>
     </>

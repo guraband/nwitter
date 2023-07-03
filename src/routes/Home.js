d1f00c057e4920a3371b9f866b4c5b0f1bc8dc4a
NG <guraband@naver.com>
2023-07-03 23:57:54 +0900

    작성 목록 불러오기 구현

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,8 +1,24 @@
 import { dbService } from "fbInstance";
-import { React, useState } from "react";
+import { React, useState, useEffect } from "react";
 
 const Home = () => {
     const [nweet, setNweet] = useState("");
+    const [nweets, setNweets] = useState([]);
+
+    const getNweets = async () => {
+        const dbNweets = await dbService.collection("nweets").get();
+        dbNweets.forEach((document) => {
+            const nweetObject = {
+                ...document.data(),
+                id: document.id,
+            }
+            setNweets((prev) => [nweetObject, ...prev]);
+        });
+    }
+
+    useEffect(() => {
+        getNweets();
+    }, []);
 
     const onSubmit = async (event) => {
         event.preventDefault();
@@ -20,11 +36,23 @@ const Home = () => {
         setNweet(value);
     }
 
+    console.log(nweets);
+
     return (<>
         <form onSubmit={onSubmit}>
             <input type="text" placeholder="What's on your mind?" maxLength={120} onChange={onChange} value={nweet} />
             <input type="submit" value="Nweet" />
         </form>
+
+        <div>
+            <ul>
+                {nweets.map(nweet => (
+                    <li key={nweet.id}>
+                        <h4>{nweet.nweet}</h4>
+                    </li>
+                ))}
+            </ul>
+        </div>
     </>
     );
 }
